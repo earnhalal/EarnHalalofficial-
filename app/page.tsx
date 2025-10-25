e205a4caaddf329605fdc9a23de08005fab1bb6a
earnhalal <r83842009@gmail.com>
2025-10-25 22:38:54 +0500

    app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign:"center", marginTop:"100px"}}>
+      <h1>Assalam Alaikum!</h1>
+      <p><a href="/login" style={{color:"#007bff"}}>Login karo</a></p>
+    </div>
+  );
+}
